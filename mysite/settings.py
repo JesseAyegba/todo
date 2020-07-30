0eeb2fe0788057eb36a6372d3cba4619f6b315b5
Jesse Ayegba <jessejoseph64@gmail.com>
2020-07-30 06:43:26 +0100

    Set Debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["TODO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ["TODO_DEBUG_VALUE"] == "True")
 
 ALLOWED_HOSTS = ["todolistbyjesse.herokuapp.com"]
 
