9ebd9976cfe1f498a6807b5eb1f7cd97e3675263
Ayegba Jesse <jessejoseph64@gmail.com>
2020-08-17 01:27:16 +0100

    Final profile fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,4 +150,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_LOCATION = "media"
 # STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
